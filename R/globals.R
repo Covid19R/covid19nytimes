e65e47833fe47c67a41b895e314951c6702bb625
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-03-29 14:00:01 -0400

    Adding global variables.

---
+++ R/globals.R
@@ -0,0 +1,25 @@
+globalVariables(
+  c(
+    #standard for all datasets
+    "date",
+    "location",
+    "location_type",
+    "location_standardized",
+    "location_standardized_type",
+    "data_type",
+    "value",
+    "data_set_name",
+    "package_name",
+    "function_to_get_data",
+    "data_details",
+    "data_url",
+    "license_url",
+    "data_types",
+    "has_geospatial_info",
+
+    #unique to this dataset
+    "cases",
+    "cases_total",
+    "deaths_total"
+  )
+)
