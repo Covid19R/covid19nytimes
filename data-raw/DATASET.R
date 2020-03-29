ae3ee9b4744a8680fa410ca4f31843129f7a95e3
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-03-29 11:42:22 -0400

    Adding data update code.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,19 @@
-## code to prepare `DATASET` dataset goes here
+#---------------------------
+# Code to update NY Times Datasets
+#---------------------------
 
-usethis::use_data("DATASET")
+setwd(here::here())
+source("./R/refresh_covid19nytimes.R")
+
+covid19nytimes_states <- refresh_covid19nytimes_states()
+covid19nytimes_counties <- refresh_covid19nytimes_counties()
+
+#look
+head(covid19nytimes_states)
+head(covid19nytimes_states)
+
+#test with testthat
+
+#deploy data
+usethis::use_data(covid19nytimes_states, overwrite = TRUE)
+usethis::use_data(covid19nytimes_counties, overwrite = TRUE)
