50f2467f15a06d7d0ecbfbd28311737f60ecca46
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-04-28 22:12:10 -0400

    Update to non-nested locations

--- R/refresh_covid19nytimes.R
+++ R/refresh_covid19nytimes.R
@@ -14,7 +14,7 @@
 #' @return A tibble object
 #' * date - The date in YYYY-MM-DD form
 #' * location - The name of the location as provided by the data source. The counties dataset provides county and state. They are combined and separated by a `,`, and can be split by `tidyr::separate()`, if you wish.
-#' * location_type - The type of location using the covid19R controlled vocabulary. Nested locations are indicated by multiple location types being combined with a `_
+#' * location_type - The type of location using the covid19R controlled vocabulary.
 #' * location_code - A standardized location code using a national or international standard. In this case, FIPS state or county codes. See https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code and https://en.wikipedia.org/wiki/FIPS_county_code for more
 #' * location_code_type The type of standardized location code being used according to the covid19R controlled vocabulary. Here we use `fips_code`
 #' * data_type - the type of data in that given row. Includes `total_cases` and `total_deaths`, cumulative measures of both.
@@ -64,7 +64,7 @@ refresh_covid19nytimes_states <- function() {
 #' @return A tibble object
 #' * date - The date in YYYY-MM-DD form
 #' * location - The name of the location as provided by the data source. The counties dataset provides county and state. They are combined and separated by a `,`, and can be split by `tidyr::separate()`, if you wish.
-#' * location_type - The type of location using the covid19R controlled vocabulary. Nested locations are indicated by multiple location types being combined with a `_
+#' * location_type - The type of location using the covid19R controlled vocabulary.
 #' * location_code - A standardized location code using a national or international standard. In this case, FIPS state or county codes. See https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code and https://en.wikipedia.org/wiki/FIPS_county_code for more
 #' * location_code_type The type of standardized location code being used according to the covid19R controlled vocabulary. Here we use `fips_code`
 #' * data_type - the type of data in that given row. Includes `total_cases` and `total_deaths`, cumulative measures of both.
